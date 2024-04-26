d5ac809b2c1b6cbf31181cc4eb0cb7e30a10209b
Tulio Bitencourt <tulio.bf51@gmail.com>
2024-04-25 23:57:19 -0300

    Refactoring and Adding DB

--- main.go
+++ main.go
@@ -1,38 +1,14 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/Bitencoo/event-booking-api.git/models"
+	"github.com/Bitencoo/event-booking-api.git/db"
+	"github.com/Bitencoo/event-booking-api.git/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+  db.InitDB()
   r := gin.Default()
-  r.GET("/events", getEvents)
-  r.POST("/events", createEvent)
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-}
-
-func getEvents(c *gin.Context) {
-	events := models.GetAllEvents()
-	c.JSON(http.StatusOK, events)
-}
-
-func createEvent(c *gin.Context) {
-	var event models.Event
-	err := c.ShouldBindJSON(&event)
-
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message" : "Could not parse the data!"})
-	}
-
-	event.ID = 1
-	event.UserID = 1
-	event.Save()
-
-	c.JSON(http.StatusCreated, gin.H{
-		"message" : "Event Created!",
-		"event" : event,
-	})
+  routes.RegisterRoutes(r)
+  r.Run()
 }
--- models/event.go
+++ models/event.go
@@ -1,9 +1,13 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/Bitencoo/event-booking-api.git/db"
+)
 
 type Event struct {
-	ID int
+	ID int64
 	Name string `binding: required`
 	Description string `binding: required`
 	Location string `binding: required`
@@ -11,13 +15,110 @@ type Event struct {
 	UserID int
 }
 
-var events = []Event{}
+func (e Event) Save() error {
+	query := `
+		INSERT INTO EVENTS(name, description, location, datetime, user_id)
+		VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
 
-func (e Event) Save() {
-	//later: add to database
-	events = append(events, e)
+	if err != nil {
+		return err;
+	}
+	
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err;
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return err;
+	}
+
+	e.ID = id
+	
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM EVENTS"
+	rows, err := db.DB.Query(query)
+
+	if err != nil {
+		return nil, err;
+	}
+
+	defer rows.Close()
+
+	var events = []Event{}
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := `
+		SELECT * FROM EVENTS WHERE ID = ?
+	`
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
+}
+
+func (e Event) UpdateEvent() error{
+	query := `
+		UPDATE EVENTS 
+		SET name = ?, description = ?, location = ?, datetime = ?
+		WHERE id = ? 
+	`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.ID)
+	return err
+}
+
+func DeleteEvent(id int64) (error) {
+	query := `
+		DELETE FROM EVENTS WHERE ID = ?
+	`
+
+	stmt, err := db.DB.Prepare(query)
+	
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(id)
+
+	return err
 }
